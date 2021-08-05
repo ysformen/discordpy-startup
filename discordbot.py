66b15e6c9e7699088f52a1adfa8b27ea78863688
Itsuya Ikumi <ysformen@gmail.com>
2021-08-05 21:53:31 +0900

    test code implement

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,22 @@
-from discord.ext import commands
+#from discord.ext import commands
+import discord
+
+
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content == 'hello':
+        await message.channel.send('hi')
+
+client.run(TOKEN)
+"""
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -23,3 +35,4 @@ async def yani(ctx):
     await ctx.send('億トレーダーですよね')
 
 bot.run(token)
+"""
